4cf768600e17afa48f5f0879ec60f0bfbb6c0118
Chinmay Chandak <chandakchinmay@gmail.com>
2019-03-06 22:48:00 +0530

    added EnemyManager
    Signed-off-by: Chinmay Chandak, @CCAtAlvis

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    //public int minEnemies = 3;
+    //public int maxEnemies = 10;
+
+    //public int numOfEnemies = 0;
+    public int startTime = 1;
+    public int delayTime = 3;
+    public int matchTime = 8;
+
+    private int playerId = 1;
+
+    public Transform[] spawnPoint;
+    public GameObject enemyPrefab;
+    private bool waiting;
+
+    // Use this for initialization
+    void Start()
+    {
+        matchTime *= 60;
+        InvokeRepeating("SpawnEnemy", startTime, delayTime);
+    }
+
+    void SpawnEnemy()
+    {
+        int i = Random.Range(0, spawnPoint.Length);
+        //Debug.Log("spwaning enemy at position: " + i);
+
+        GameObject newEnemy = Instantiate(enemyPrefab, spawnPoint[i].position, Quaternion.identity);
+        newEnemy.GetComponent<EnemyAI>().followPlayer = playerId;
+        playerId *= -1;
+    }
+}
