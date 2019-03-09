6c1c50972bb6e47ff65721ebf664c91320e654a1
Janhavi <janhavi.dubule16@siesgst.ac.in>
2019-03-09 14:31:28 +0530

    added level 2

---
+++ Assets/Scripts/BossManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossManager : MonoBehaviour
+{
+    public EnemyManager enemyManager;
+    public GameObject bossPrefab;
+    public GameObject spawnPoint;
+    private bool canSpawnBoss = false;
+    private bool notSpawnBoss = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        canSpawnBoss = enemyManager.cameraTransition;
+        if(canSpawnBoss && notSpawnBoss)
+        {
+            SpawnBoss();
+            notSpawnBoss = false;
+        }
+
+
+    }
+
+    void SpawnBoss()
+    {
+        GameObject boss = Instantiate(bossPrefab, spawnPoint.transform.position, Quaternion.identity);
+    }
+}
