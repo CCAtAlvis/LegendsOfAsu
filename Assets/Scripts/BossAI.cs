e4957ff75cb28ab8a51a59a3f78a8c0dc17bbe64
Chinmay Chandak <chandakchinmay@gmail.com>
2019-03-09 15:24:21 +0530

    working on boss 2

--- Assets/Scripts/BossAI.cs
+++ Assets/Scripts/BossAI.cs
@@ -1,19 +1,70 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BossAI : MonoBehaviour
 {
-    Animator animator;
+    public Animator animator;
+    public float bossHealth;
+    public int hitDamage;
+    public float deadAnimTime;
+    public float takeDamagePauseTime;
+
+    private bool isBossPaused;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("DoAttack", 20f, 15f);
+        InvokeRepeating("DoJump", 15f, 7f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void DoJump()
     {
-        
+        if (isBossPaused)
+            return;
+
+        //TODO: JUMP
+        //flip
+
+        animator.SetTrigger("jump");
+    }
+
+    void DoAttack()
+    {
+        if (isBossPaused)
+            return;
+
+        Collider2D[] enemiesToHit = Physics2D.OverlapCircleAll(allSideAttackPos.position, allSideAttackRadius, whatIsEnemies);
+        for (int i = 0; i < enemiesToHit.Length; i++)
+        {
+            PlayerController pc = enemiesToHit[i].gameObject.GetComponent<PlayerController>();
+            pc.TakeDamage(hitDamage);
+      
+        }
+
+        animator.SetTrigger("attack");
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        bossHealth -= damageAmount;
+
+        if (bossHealth <= 0)
+        {
+            //Pause game here
+        }
+
+        StartCoroutine(PauseBossMovement(takeDamagePauseTime));
+    }
+
+    IEnumerator PauseBossMovement(float pauseDuration)
+    {
+        isBossPaused = true;
+        animator.SetBool("idle", true);
+
+        yield return new WaitForSeconds(pauseDuration);
+
+        isBossPaused = false;
+        animator.SetBool("idle", false);
     }
 }
